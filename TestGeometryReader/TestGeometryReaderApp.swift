204bac6702fe1a114282165597d13650660a58a8
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-06 15:33:44 +0200

    Initial Commit

---
+++ TestGeometryReader/TestGeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGeometryReaderApp.swift
+//  TestGeometryReader
+//
+//  Created by Kaori Persson on 2022-10-06.
+//
+
+import SwiftUI
+
+@main
+struct TestGeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
